3bcf9f1a9a0180231181861f606ad6f050832ef0
Jules <juleshaezebrouck@hotmail.com>
2017-10-31 00:00:56 +0100

    added query and table creator for selection

--- scripts/train.js
+++ scripts/train.js
@@ -11,3 +11,45 @@ function setOverlay(bool){
         document.getElementById("overlay-window").style.display = "none";
     }
 }
+
+function querySelectedTrains(){
+    from = $("#fromStation").val();
+    to = $("#toStation").val();
+    rawtime = $("#timeSelect").val();
+    time =  rawtime.substring(0,2) + rawtime.substring(3,5);
+
+    $.ajax({
+    type: "GET",
+    url: "https://api.irail.be/connections/",
+    data:{
+        from: from,
+        to: to,
+        time: time,
+        format: "json"
+    },
+    dataType: "json",
+    success: printSelectedTrains,
+    error: function(req, e, httpe){
+        alert(e+"\n"+httpe);
+    }
+    });
+}
+
+function printSelectedTrains(query){
+    //alert(typeof(query));
+    connections = query["connection"];
+    $("#trainSelectResult").html="";
+    $("#trainSelectResult").append(
+        $(document.createElement("table"))
+        .addClass("selectedTrainResult")
+    );
+    for(i = 0;i<connections.length;i++){
+        $(".selectedTrainResult").append(
+            $(document.createElement("tr")).append(
+                $(document.createElement("td")).html(
+                    connections[i].departure.stationinfo.id
+                )
+            )
+        );
+    }
+}
